e285d106eea7135eeb42da5d09bb44721d610c3f
jose.bento <jose.natal@ibrowse-sds.com.br>
2025-01-28 19:28:37 -0300

    Resgatando foto por id e atualizando title

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -86,9 +86,78 @@ const getUserPhotos = async (req, res) => {
     return res.status(200).json(photos);
   };
 
+  // Get photo by id
+const getPhotoById = async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+        const photo = await Photo.findById(id);
+    
+        // Check if photo exists
+        if (!photo) {
+            res.status(404).json({ errors: ["Foto não encontrada!"] });
+            return;
+        }
+        res.status(200).json(photo);
+    } catch (error) {
+        res.status(404).json({ errors: ["Foto não encontrada!"] });
+        return;
+    }
+  };
+
+  // Update a photo
+const updatePhoto = async (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+  
+    let image;
+  
+    if (req.file) {
+      image = req.file.filename;
+    }
+  
+    const reqUser = req.user;
+  
+    try {
+        const photo = await Photo.findById(id);
+    
+        // Check if photo exists
+        if (!photo) {
+            res.status(404).json({ errors: ["Foto não encontrada!"] });
+            return;
+        }
+    
+        // Check if photo belongs to user
+        if (!photo.userId.equals(reqUser._id)) {
+            res
+                .status(422)
+                .json({ errors: ["Ocorreu um erro, tente novamente mais tarde"] });
+            return;
+        }
+    
+        if (title) {
+            photo.title = title;
+        }
+    
+        if (image) {
+            photo.image = image;
+        }
+    
+        await photo.save();
+    
+        return res.status(200).json({ photo, message: "Foto atualizada com sucesso!" });
+    } catch (error) {
+        res.status(404).json({ errors: ["Foto não encontrada!"] });
+        return;
+    }
+  };
+
+
 module.exports = {
     insertPhoto,
     deletePhoto,
     getAllPhotos,
     getUserPhotos,
+    getPhotoById,
+    updatePhoto,
   };
--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -6,11 +6,13 @@ const {
     insertPhoto, 
     deletePhoto, 
     getAllPhotos, 
-    getUserPhotos 
+    getUserPhotos,
+    getPhotoById,
+    updatePhoto,
 } = require("../controllers/PhotoController");
 
 // Middleware
-const { photoInsertValidation } = require("../middlewares/photoValidation")
+const { photoInsertValidation, photoUpdateValidation } = require("../middlewares/photoValidation")
 const authGuard = require("../middlewares/authGuard")
 const validate = require("../middlewares/handleValidation")
 const { imageUpload } = require("../middlewares/imageUpload")
@@ -28,5 +30,7 @@ router.post(
 router.delete("/:id", authGuard, deletePhoto);
 router.get("/", authGuard, getAllPhotos);
 router.get("/user/:id", authGuard, getUserPhotos);
+router.get("/:id", authGuard, getPhotoById);
+router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
 
 module.exports = router;
