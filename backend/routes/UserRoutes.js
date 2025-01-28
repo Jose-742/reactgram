16fa7933fd99c275f3cc51f9131fe5df0771929c
jose.bento <jose.natal@ibrowse-sds.com.br>
2025-01-28 18:14:50 -0300

    Resgatando usuário pelo id

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -7,6 +7,7 @@ const {
     login, 
     getCurrentUser,
     update,
+    getUserById,
 } = require("../controllers/UserController")
 
 // Middlewares
@@ -24,5 +25,6 @@ router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
 router.get("/profile", authGuard, getCurrentUser);
 router.put("/", authGuard, userUpdatevalidation(), validate, imageUpload.single("profileImage"), update)
+router.get("/:id", getUserById);
 
 module.exports = router;
