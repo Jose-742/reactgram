7276068b4f1a0970386922435dd1d773c6b7ccb3
jose.bento <jose.natal@ibrowse-sds.com.br>
2025-01-30 20:52:13 -0300

    Criando componentes de navbar e footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,10 @@ import './App.css'
 //Router
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
+//components
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 //pages
 import Home from './pages/Home/Home'
 import Login from './pages/Auth/Login'
@@ -13,11 +17,15 @@ function App() {
   return (
     <div className='App'>
       <BrowserRouter>
-          <Routes>
-              <Route path='/' element={<Home/>}/>
-              <Route path='/login' element={<Login/>}/>
-              <Route path='/register' element={<Register/>}/>
-          </Routes>
+          <Navbar />
+          <div className="container">
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/register' element={<Register/>}/>
+            </Routes>
+          </div>
+          <Footer/>
       </BrowserRouter>
     </div>
   )
